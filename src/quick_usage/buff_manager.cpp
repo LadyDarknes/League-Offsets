6679763d5cb9c5f6abdcd1c0c68b819b645925f4
LadyDarknes <85314429+LadyDarknes@users.noreply.github.com>
2026-05-27 12:00:37 +0300

    Update - ~0,4-~4,2-~6,2 ~8,2:~10,2

--- src/quick_usage/buff_manager.cpp
+++
@@ -1,7 +0,0 @@
-// BuffManager Direct Access (Bypassing Wrapper)
-
-void* buff_mgr = *(void**)((char*)obj + 0x28F0);
-if (buff_mgr) {
-    void* list_start = *(void**)((char*)buff_mgr + 0x90);
-    void* list_end = *(void**)((char*)buff_mgr + 0x98);
-}
