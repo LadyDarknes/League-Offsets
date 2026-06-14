d49616b64a404cb0fdff19750b76aebe7f094c7b
LadyDarknes <85314429+LadyDarknes@users.noreply.github.com>
2026-06-14 16:30:29 +0300

    New analysis for AIManager

--- src/quick_usage/entity_list.cpp
+++ src/quick_usage/entity_list.cpp
@@ -160,21 +160,19 @@ void ProcessEntitiesExample(uintptr_t base_addr) {
         void* ai_mgr_wrapper = *(void**)((char*)obj + 0x4070);
         if (ai_mgr_wrapper) {
             void* ai_mgr = *(void**)((char*)ai_mgr_wrapper + 0x28);
-            if (ai_mgr) {
-                bool is_moving = *(bool*)((char*)ai_mgr + 0x31C);
-                bool is_dashing = *(bool*)((char*)ai_mgr + 0x384);
-                float dash_speed = *(float*)((char*)ai_mgr + 0x360);
-                Vec3 target_pos = *(Vec3*)((char*)ai_mgr + 0x34);
-                Vec3 velocity = *(Vec3*)((char*)ai_mgr + 0x318);
-                Vec3 server_pos = *(Vec3*)((char*)ai_mgr + 0x474);
+            void* nav_inner = *(void**)((char*)ai_mgr_wrapper + 0x40);
+            if (ai_mgr && nav_inner) {
+                bool is_moving = *(bool*)((char*)nav_inner + 0x320);
+                bool is_dashing = *(bool*)((char*)nav_inner + 0x348);
+                float dash_speed = *(float*)((char*)nav_inner + 0x3E0);
+                Vec3 target_pos = *(Vec3*)((char*)ai_mgr + 0x24);
+                Vec3 velocity = *(Vec3*)((char*)ai_mgr + 0x18);
+                Vec3 server_pos = *(Vec3*)((char*)ai_mgr + 0x08);
 
-                void* nav_inner = *(void**)((char*)ai_mgr + 0x40);
-                if (nav_inner) {
-                    void* nav_path = (char*)nav_inner + 0x45C;
-                    int current_node_idx = *(int*)((char*)nav_path + 0x00);
-                    int waypoint_count = *(int*)((char*)nav_path + 0x30);
-                    Vec3* waypoints = *(Vec3**)((char*)nav_path + 0x28);
-                }
+                void* nav_path = (char*)nav_inner + 0x490;
+                int current_node_idx = *(int*)((char*)nav_path + 0x00);
+                int waypoint_count = *(int*)((char*)nav_path + 0x30);
+                Vec3* waypoints = *(Vec3**)((char*)nav_path + 0x28);
             }
         }
     }
