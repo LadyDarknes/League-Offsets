ae9446f9302872fbd4491f66f93b40baae4f742d
LadyDarknes <sptovaknjo@gmail.com>
2026-06-17 04:27:04 +0300

    Mini patch

--- src/offsets.hpp
+++ src/offsets.hpp
@@ -2,7 +2,7 @@
 #include <cstdint>
 
 namespace Offsets {
-    constexpr uintptr_t ImageBase = 0x7FF69D8A0000;
+    constexpr uintptr_t ImageBase = 0x7FF640FB0000;
 
     namespace Globals {
         constexpr uintptr_t CastSpellFlag = 0x1DD8F70;
@@ -28,58 +28,58 @@ namespace Offsets {
     }
 
     namespace Functions {
-        constexpr uintptr_t BuffManagerClient__OnBuffAdd = 0x93DFB0;
-        constexpr uintptr_t BuffManagerClient__OnBuffRemove = 0x93EB00;
+        constexpr uintptr_t BuffManagerClient__OnBuffAdd = 0x93DFD0;
+        constexpr uintptr_t BuffManagerClient__OnBuffRemove = 0x93EB20;
         constexpr uintptr_t CastSpellWrapper = 0x20AC40;
-        constexpr uintptr_t ClientMainLoop = 0x5EEC00;
-        constexpr uintptr_t ComponentLookupFn = 0x1189220;
-        constexpr uintptr_t ComponentRegistry__Lookup = 0x1189220;
+        constexpr uintptr_t ClientMainLoop = 0x5EEC20;
+        constexpr uintptr_t ComponentLookupFn = 0x11892B0;
+        constexpr uintptr_t ComponentRegistry__Lookup = 0x11892B0;
         constexpr uintptr_t CurrentNodeIdx = 0x3906F;
         constexpr uintptr_t DistanceSq = 0x2205F0;
         constexpr uintptr_t GameObject__IsType = 0x28F150;
         constexpr uintptr_t GetAIManager = 0x27D9D0;
-        constexpr uintptr_t GetAttackDelay = 0x5726A0;
-        constexpr uintptr_t GetAttackWindup = 0x5725A0;
+        constexpr uintptr_t GetAttackDelay = 0x5726B0;
+        constexpr uintptr_t GetAttackWindup = 0x5725B0;
         constexpr uintptr_t GetBoundingRadius = 0x27EB60;
-        constexpr uintptr_t GetCollisionFlags = 0x1243CA0;
-        constexpr uintptr_t GetFirstObject = 0x54FA40;
+        constexpr uintptr_t GetCollisionFlags = 0x1243D30;
+        constexpr uintptr_t GetFirstObject = 0x54FA50;
         constexpr uintptr_t GetMapID = 0x2852C0;
-        constexpr uintptr_t GetNextObject = 0x5509D0;
-        constexpr uintptr_t GetPing = 0x6C1760;
-        constexpr uintptr_t GetSpellCastInfo = 0x652CA0;
-        constexpr uintptr_t GetSpellSlot = 0x96E160;
-        constexpr uintptr_t GetSubObjAiMgr = 0x539AD0;
+        constexpr uintptr_t GetNextObject = 0x5509E0;
+        constexpr uintptr_t GetPing = 0x6C1780;
+        constexpr uintptr_t GetSpellCastInfo = 0x652CC0;
+        constexpr uintptr_t GetSpellSlot = 0x96E180;
+        constexpr uintptr_t GetSubObjAiMgr = 0x539AE0;
         constexpr uintptr_t IsAlive = 0x2B79D0;
         constexpr uintptr_t IsBuilding = 0x2CCD90;
         constexpr uintptr_t IsHero = 0x2CCBE0;
         constexpr uintptr_t IsTurret = 0x2CCAE0;
         constexpr uintptr_t IsVisible = 0x217F90;
         constexpr uintptr_t IssueOrderCore = 0x290940;
-        constexpr uintptr_t MinimapHudInit = 0x940BF0;
-        constexpr uintptr_t MinimapMoveSelf = 0x57A7E0;
-        constexpr uintptr_t NavAgent = 0x591F10;
-        constexpr uintptr_t OnCreateObject = 0x55B330;
-        constexpr uintptr_t OnDeleteObject = 0x5612E0;
-        constexpr uintptr_t OnDoCast = 0x97F2F0;
+        constexpr uintptr_t MinimapHudInit = 0x940C10;
+        constexpr uintptr_t MinimapMoveSelf = 0x57A7F0;
+        constexpr uintptr_t NavAgent = 0x591F40;
+        constexpr uintptr_t OnCreateObject = 0x55B340;
+        constexpr uintptr_t OnDeleteObject = 0x5612F0;
+        constexpr uintptr_t OnDoCast = 0x97F310;
         constexpr uintptr_t OnPlayAnimation = 0x29BF90;
-        constexpr uintptr_t OnProcessSpell = 0x984D20;
-        constexpr uintptr_t OnSpellImpact = 0x97D6E0;
-        constexpr uintptr_t OnStopCast = 0x985460;
-        constexpr uintptr_t OnWndProc = 0x5F0780;
-        constexpr uintptr_t PrintChat = 0x112C8E0;
-        constexpr uintptr_t ProjectAndClip = 0x12F69B0;
-        constexpr uintptr_t QuestProgress__Calculate = 0x51E700;
-        constexpr uintptr_t QuestProgress__GetQuestValue = 0x5237E0;
-        constexpr uintptr_t QuestProgressFn = 0x51E700;
-        constexpr uintptr_t QuestValueFn = 0x5237E0;
-        constexpr uintptr_t RoleBound__Init = 0x10FBDB0;
-        constexpr uintptr_t RoleBoundInit = 0x10FBDB0;
-        constexpr uintptr_t SpellSlot__Cast = 0x747F70;
-        constexpr uintptr_t SpellSlot__GetCooldown = 0x932C70;
-        constexpr uintptr_t SpellSlot__GetLevel = 0x9342AB;
-        constexpr uintptr_t TransformVec3 = 0x12F89F0;
-        constexpr uintptr_t WaypointCount = 0x398B0;
-        constexpr uintptr_t WorldToScreen = 0x12F6A90;
+        constexpr uintptr_t OnProcessSpell = 0x984D40;
+        constexpr uintptr_t OnSpellImpact = 0x97D700;
+        constexpr uintptr_t OnStopCast = 0x985480;
+        constexpr uintptr_t OnWndProc = 0x5F07A0;
+        constexpr uintptr_t PrintChat = 0x112C970;
+        constexpr uintptr_t ProjectAndClip = 0x12F6A40;
+        constexpr uintptr_t QuestProgress__Calculate = 0x51E710;
+        constexpr uintptr_t QuestProgress__GetQuestValue = 0x5237F0;
+        constexpr uintptr_t QuestProgressFn = 0x51E710;
+        constexpr uintptr_t QuestValueFn = 0x5237F0;
+        constexpr uintptr_t RoleBound__Init = 0x10FBE40;
+        constexpr uintptr_t RoleBoundInit = 0x10FBE40;
+        constexpr uintptr_t SpellSlot__Cast = 0x747F90;
+        constexpr uintptr_t SpellSlot__GetCooldown = 0x932C90;
+        constexpr uintptr_t SpellSlot__GetLevel = 0x9342CB;
+        constexpr uintptr_t TransformVec3 = 0x12F8A80;
+        constexpr uintptr_t WaypointCount = 0x399F1;
+        constexpr uintptr_t WorldToScreen = 0x12F6B20;
     }
 
     namespace Reflections {
