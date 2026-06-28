b5200e1f78832f7c07188684713b430f7fc4f1a6
LadyDarknes <85314429+LadyDarknes@users.noreply.github.com>
2026-06-28 05:11:30 +0300

    new patterns

--- src/offsets.hpp
+++ src/offsets.hpp
@@ -109,12 +109,43 @@ namespace Offsets {
             constexpr uintptr_t oChargeCooldown = 0x68;
             constexpr uintptr_t oChargeLevel = 0x6C;
             constexpr uintptr_t oChargeStartTime = 0x74;
+            constexpr uintptr_t ActiveSpellCast = 0x118;
+            constexpr uintptr_t ChargeCooldownDuration = 0x6C;
+            constexpr uintptr_t ChargeCooldownExpires = 0x68;
+            constexpr uintptr_t Cooldown = 0x80;
+            constexpr uintptr_t CooldownDuration = 0x74;
+            constexpr uintptr_t CooldownExpires = 0x30;
+            constexpr uintptr_t CooldownOverride = 0x88;
+            constexpr uintptr_t Level = 0x28;
+            constexpr uintptr_t MaxStacks = 0x64;
+            constexpr uintptr_t Owner = 0x8;
+            constexpr uintptr_t SlotType = 0x2F;
+            constexpr uintptr_t SpellInfo = 0x128;
+            constexpr uintptr_t SpellInstanceVars = 0x108;
+            constexpr uintptr_t SpellNameHash = 0x110;
+            constexpr uintptr_t Stacks = 0x5C;
+            constexpr uintptr_t Vtable = 0x0;
         }
         namespace AIManager {
             constexpr uintptr_t oServerPos = 0x8;
             constexpr uintptr_t oVelocity = 0x18;
             constexpr uintptr_t oTargetPosition = 0x24;
             constexpr uintptr_t oNavPath = 0x40;
+            constexpr uintptr_t AiManager = 0x4230;
+            constexpr uintptr_t CurrentSegment = 0x320;
+            constexpr uintptr_t DashSpeed = 0x360;
+            constexpr uintptr_t IsDashing = 0x384;
+            constexpr uintptr_t IsMoving = 0x31C;
+            constexpr uintptr_t MoveVec3 = 0x480;
+            constexpr uintptr_t NavArray = 0x348;
+            constexpr uintptr_t ObjectOffset = 0x4230;
+            constexpr uintptr_t PathState = 0x320;
+            constexpr uintptr_t SegmentsCount = 0x350;
+            constexpr uintptr_t ServerPos = 0x474;
+            constexpr uintptr_t StartPath = 0x330;
+            constexpr uintptr_t TargetPos = 0x34;
+            constexpr uintptr_t TargetPosition = 0x33C;
+            constexpr uintptr_t Velocity = 0x318;
         }
         namespace CharacterData {
             constexpr uintptr_t oSkinID = 0x14;
@@ -124,8 +155,8 @@ namespace Offsets {
         namespace MissileClient {
             constexpr uintptr_t CasterNetID = 0x30;
             constexpr uintptr_t TargetNetID = 0x34;
-            constexpr uintptr_t StartPos = 0x24;
-            constexpr uintptr_t EndPos = 0x30;
+            constexpr uintptr_t StartPos = 0x390;
+            constexpr uintptr_t EndPos = 0x39C;
             constexpr uintptr_t Speed = 0x410;
             constexpr uintptr_t CurrentPos = 0x25C;
             constexpr uintptr_t oCasterNetID = 0x30;
@@ -133,6 +164,16 @@ namespace Offsets {
             constexpr uintptr_t oStartPos = 0x24;
             constexpr uintptr_t oEndPos = 0x30;
             constexpr uintptr_t oSpeed = 0x0;
+            constexpr uintptr_t CastEndPos = 0x3A8;
+            constexpr uintptr_t CastInfoBase = 0x2C0;
+            constexpr uintptr_t CasterIndex = 0x360;
+            constexpr uintptr_t MissileName = 0x308;
+            constexpr uintptr_t MissileNetId = 0x36C;
+            constexpr uintptr_t Position = 0x25C;
+            constexpr uintptr_t SpellDataPtr = 0x2C0;
+            constexpr uintptr_t SpellName = 0x2E0;
+            constexpr uintptr_t StartTime = 0x478;
+            constexpr uintptr_t TargetIndex = 0x35C;
         }
         namespace CombatStats {
             constexpr uintptr_t mFlatAPMod = 0x1C;
@@ -162,6 +203,65 @@ namespace Offsets {
             constexpr uintptr_t SkinId = 0x1334;
             constexpr uintptr_t CharacterData = 0x4108;
             constexpr uintptr_t oCharacterDataStackBase = 0x2C88;
+            constexpr uintptr_t BaseGoldOnDeath = 0x55B8;
+            constexpr uintptr_t Exp = 0x4D38;
+            constexpr uintptr_t Gold = 0x2868;
+            constexpr uintptr_t GoldTotal = 0x2890;
+            constexpr uintptr_t LevelRef = 0x4D60;
+            constexpr uintptr_t LevelUpPoints = 0x4D88;
+            constexpr uintptr_t NeutralMinionsKilled = 0x55E0;
+            constexpr uintptr_t RuneManager = 0x50E8;
+            constexpr uintptr_t ShutdownValue = 0x5590;
+            constexpr uintptr_t VisionScore = 0x5568;
+            constexpr uintptr_t ArrayBegin = 0x18;
+            constexpr uintptr_t ArrayCapacity = 0x28;
+            constexpr uintptr_t ArrayEnd = 0x20;
+            constexpr uintptr_t BuffManager = 0x2E78;
+            constexpr uintptr_t Count = 0x78;
+            constexpr uintptr_t Duration = 0x20;
+            constexpr uintptr_t EndTime = 0x1C;
+            constexpr uintptr_t EntryBuffInstance = 0x0;
+            constexpr uintptr_t EntryRefCount = 0x8;
+            constexpr uintptr_t EntryStride = 0x10;
+            constexpr uintptr_t InstanceVtable = 0x0;
+            constexpr uintptr_t InstanceVtableOnClearFn = 0x10;
+            constexpr uintptr_t InstanceVtableUpdateStackFn = 0x20;
+            constexpr uintptr_t IsCleared = 0x88;
+            constexpr uintptr_t LookupMap = 0x38;
+            constexpr uintptr_t OwnerNameData = 0x8;
+            constexpr uintptr_t OwnerNetworkId = 0x10;
+            constexpr uintptr_t PointerArrayBegin = 0x48;
+            constexpr uintptr_t PointerArrayCapacity = 0x58;
+            constexpr uintptr_t PointerArrayEnd = 0x50;
+            constexpr uintptr_t PointerArrayStride = 0x8;
+            constexpr uintptr_t Script = 0x10;
+            constexpr uintptr_t ScriptBuffType = 0x100;
+            constexpr uintptr_t ScriptBuffTypeAlt = 0x101;
+            constexpr uintptr_t ScriptName = 0x8;
+            constexpr uintptr_t ScriptNameHash = 0x18;
+            constexpr uintptr_t SourceNetworkId = 0x14;
+            constexpr uintptr_t StackDataPtr = 0x0;
+            constexpr uintptr_t StackDataSourceId = 0x4;
+            constexpr uintptr_t StackStride = 0x10;
+            constexpr uintptr_t StacksArray = 0x30;
+            constexpr uintptr_t StacksCount = 0x38;
+            constexpr uintptr_t StartTime = 0x18;
+            constexpr uintptr_t StateArrayBegin = 0x70;
+            constexpr uintptr_t StateArrayCapacity = 0x80;
+            constexpr uintptr_t StateArrayEnd = 0x78;
+            constexpr uintptr_t StateArrayStride = 0x68;
+            constexpr uintptr_t Type = 0xC;
+            constexpr uintptr_t Vtable = 0x0;
+            constexpr uintptr_t VtableHasBuffOfTypeFn = 0x8;
+            constexpr uintptr_t VtableUpdateStackFn = 0x20;
+            constexpr uintptr_t Avatar = 0x50A8;
+            constexpr uintptr_t BaseGoldGivenOnDeath = 0x55B8;
+            constexpr uintptr_t CombatType = 0x2CD0;
+            constexpr uintptr_t Experience = 0x4D38;
+            constexpr uintptr_t Level = 0x4D60;
+            constexpr uintptr_t MinimumGold = 0x28B8;
+            constexpr uintptr_t SkillUpLevelDelta = 0x4D88;
+            constexpr uintptr_t SpellTrainingPoints = 0x4D88;
         }
         namespace GameObject {
             constexpr uintptr_t oIndex = 0x20;
@@ -213,14 +313,119 @@ namespace Offsets {
             constexpr uintptr_t OFF_SPELLBOOK = 0x3128;
             constexpr uintptr_t OFF_AIMANAGER_PTR = 0x4070;
             constexpr uintptr_t OFF_HEROCLIENT_PTR = 0x4230;
+            constexpr uintptr_t AbilityHaste = 0x1BC0;
+            constexpr uintptr_t Armor = 0x2098;
+            constexpr uintptr_t AttackRange = 0x21D8;
+            constexpr uintptr_t AttackSpeedMod = 0x1E68;
+            constexpr uintptr_t BaseAttackDamage = 0x1F08;
+            constexpr uintptr_t Crit = 0x2048;
+            constexpr uintptr_t CritDamageMultiplier = 0x1FD0;
+            constexpr uintptr_t FlatArmorPen = 0x2250;
+            constexpr uintptr_t FlatMagicPen = 0x2340;
+            constexpr uintptr_t FlatPhysicalDmgMod = 0x1D00;
+            constexpr uintptr_t HPRegenRate = 0x2138;
+            constexpr uintptr_t MP = 0x360;
+            constexpr uintptr_t MagicLethality = 0x2368;
+            constexpr uintptr_t MaxMP = 0x388;
+            constexpr uintptr_t MoveSpeed = 0x2188;
+            constexpr uintptr_t PercentArmorPen = 0x22A0;
+            constexpr uintptr_t PercentAttackSpeedMod = 0x1E90;
+            constexpr uintptr_t PercentBonusArmorPen = 0x22C8;
+            constexpr uintptr_t PercentBonusMagicPen = 0x23B8;
+            constexpr uintptr_t PercentCCReduction = 0x24A8;
+            constexpr uintptr_t PercentLifeSteal = 0x23E0;
+            constexpr uintptr_t PercentMagicPen = 0x2390;
+            constexpr uintptr_t PercentOmnivamp = 0x2430;
+            constexpr uintptr_t PercentSpellVamp = 0x2408;
+            constexpr uintptr_t PhysicalLethality = 0x2278;
+            constexpr uintptr_t SpellBlock = 0x20E8;
+            constexpr uintptr_t CharacterName = 0x4370;
+            constexpr uintptr_t Name = 0x4370;
+            constexpr uintptr_t AllShield = 0x1120;
+            constexpr uintptr_t HP = 0x1080;
+            constexpr uintptr_t IsTargetable = 0xED0;
+            constexpr uintptr_t MagicalShield = 0x1170;
+            constexpr uintptr_t MaxHP = 0x10A8;
+            constexpr uintptr_t PhysicalShield = 0x1148;
+            constexpr uintptr_t TargetableFlags = 0xEF8;
+            constexpr uintptr_t AbilityPower = 0x1DA0;
+            constexpr uintptr_t ActionState = 0x14A0;
+            constexpr uintptr_t ActionState2 = 0x14C8;
+            constexpr uintptr_t ActionStateBase = 0x1470;
+            constexpr uintptr_t BonusAttackDamage = 0x1D00;
+            constexpr uintptr_t ChampionName = 0x6B8;
+            constexpr uintptr_t CharData = 0x4078;
+            constexpr uintptr_t CharacterActionState = 0x14C8;
+            constexpr uintptr_t CharacterDataStack = 0x4108;
+            constexpr uintptr_t CharacterIntermediate = 0x1B98;
+            constexpr uintptr_t CharacterState = 0x14A0;
+            constexpr uintptr_t CombatStatsBase = 0x1B98;
+            constexpr uintptr_t DirectionComponent = 0x1288;
+            constexpr uintptr_t DirectionVFunc = 0xA8;
+            constexpr uintptr_t DirectionVector = 0x20;
+            constexpr uintptr_t EvolvePoints = 0x4C48;
+            constexpr uintptr_t FlatArmorPenetration = 0x2250;
+            constexpr uintptr_t FlatMagicPenetration = 0x2340;
+            constexpr uintptr_t Health = 0x1080;
+            constexpr uintptr_t Index = 0x20;
+            constexpr uintptr_t Inventory = 0x4DC8;
+            constexpr uintptr_t IsBotVFunc = 0x770;
+            constexpr uintptr_t IsTargetableToTeamFlags = 0xEF8;
+            constexpr uintptr_t IsVisible = 0x310;
+            constexpr uintptr_t IsZombie = 0x3A;
+            constexpr uintptr_t Mana = 0x360;
+            constexpr uintptr_t MaxHealth = 0x10A8;
+            constexpr uintptr_t MaxMana = 0x388;
+            constexpr uintptr_t OverrideCollisionHeight = 0x88;
+            constexpr uintptr_t OverrideCollisionRadius = 0x758;
+            constexpr uintptr_t PARRegenRate = 0x2548;
+            constexpr uintptr_t PARState = 0x428;
+            constexpr uintptr_t PathfindingCollisionRadius = 0x758;
+            constexpr uintptr_t PathfindingRadiusMod = 0x2480;
+            constexpr uintptr_t PercentArmorPenetration = 0x22A0;
+            constexpr uintptr_t PercentBonusArmorPenetration = 0x22C8;
+            constexpr uintptr_t PercentBonusMagicPenetration = 0x23B8;
+            constexpr uintptr_t PercentHealingAmountMod = 0x1EE0;
+            constexpr uintptr_t PercentLifeStealMod = 0x23E0;
+            constexpr uintptr_t PercentMagicDamageMod = 0x1DC8;
+            constexpr uintptr_t PercentMagicPenetration = 0x2390;
+            constexpr uintptr_t PercentOmnivampMod = 0x2430;
+            constexpr uintptr_t PercentPhysicalDamageMod = 0x1D28;
+            constexpr uintptr_t PercentPhysicalVamp = 0x2458;
+            constexpr uintptr_t PercentSpellVampMod = 0x2408;
+            constexpr uintptr_t Position = 0x25C;
+            constexpr uintptr_t SelectionHeight = 0x88;
+            constexpr uintptr_t SelectionRadius = 0x8C;
+            constexpr uintptr_t SkinName = 0x4370;
+            constexpr uintptr_t StatusFlags = 0x5C8;
+            constexpr uintptr_t StatusFlagsInvulnerableMask = 0x1;
+            constexpr uintptr_t Team = 0x259;
         }
         namespace SpellDataResource {
             constexpr uintptr_t oMissileSpeed = 0x408;
+            constexpr uintptr_t DataResourceBase = 0x60;
+            constexpr uintptr_t ResAmmoRecharge = 0x408;
+            constexpr uintptr_t ResCastRange = 0x478;
+            constexpr uintptr_t ResCastRangeDisplayOverride = 0x548;
+            constexpr uintptr_t ResCastType = 0x2F;
+            constexpr uintptr_t ResCooldownTime = 0x6C8;
+            constexpr uintptr_t ResImgIconName = 0x2A0;
+            constexpr uintptr_t ResLineWidth = 0x568;
+            constexpr uintptr_t ResMaxAmmo = 0x3C0;
+            constexpr uintptr_t ResMissileSpec = 0x508;
+            constexpr uintptr_t ResMissileSpeed = 0x518;
+            constexpr uintptr_t ResScriptName = 0x80;
         }
         namespace SpellBook {
             constexpr uintptr_t oActiveSpellCast = 0xAD8;
             constexpr uintptr_t oSpellSlots = 0xAE0;
             constexpr uintptr_t OFF_SB_DELAY_PCT = 0x308;
+            constexpr uintptr_t ActiveSpellCast = 0x38;
+            constexpr uintptr_t LastSlot = 0x3F;
+            constexpr uintptr_t MaxSlots = 0x40;
+            constexpr uintptr_t SlotStride = 0x8;
+            constexpr uintptr_t SpellBook = 0x3128;
+            constexpr uintptr_t SpellSlotArray = 0xAE0;
         }
         namespace SkinResource {
             constexpr uintptr_t skin_id = 0x0;
@@ -231,6 +436,13 @@ namespace Offsets {
         }
         namespace SpellData {
             constexpr uintptr_t oName = 0x28;
+            constexpr uintptr_t InfoLevelOrFlag = 0x14;
+            constexpr uintptr_t InfoOwnerSlot = 0x60;
+            constexpr uintptr_t InfoSpellData = 0x8;
+            constexpr uintptr_t ResourceData = 0x60;
+            constexpr uintptr_t ResourceName = 0x28;
+            constexpr uintptr_t Vtable = 0x0;
+            constexpr uintptr_t VtableGetTargetingFn = 0x20;
         }
         namespace ChampionResource {
             constexpr uintptr_t skins = 0xC8;
@@ -238,12 +450,72 @@ namespace Offsets {
         namespace CameraData {
             constexpr uintptr_t oCamPos = 0x8;
             constexpr uintptr_t oCamRotation = 0x18;
-            constexpr uintptr_t oCamAttached = 0x0;
+            constexpr uintptr_t oCamAttached = 0x28;
             constexpr uintptr_t oCamMoveSpeed = 0x2C;
             constexpr uintptr_t oFieldOfView = 0x3C;
             constexpr uintptr_t oNearClip = 0x44;
             constexpr uintptr_t oFarClip = 0x4C;
             constexpr uintptr_t oFogOfWar = 0x5A;
+        }
+        namespace NavGrid {
+            constexpr uintptr_t OFF_NAV_WIDTH = 0x710;
+            constexpr uintptr_t OFF_NAV_HEIGHT = 0x714;
+            constexpr uintptr_t OFF_NAV_ORIGIN = 0xF4;
+            constexpr uintptr_t OFF_NAV_CELLSIZE = 0x71C;
+            constexpr uintptr_t OFF_NAV_DATA = 0x118;
+            constexpr uintptr_t CellArray = 0x110;
+            constexpr uintptr_t CellCenterOffset = 0x718;
+            constexpr uintptr_t CellCountX = 0x708;
+            constexpr uintptr_t CellCountZ = 0x70C;
+            constexpr uintptr_t CellDataFlags = 0x6;
+            constexpr uintptr_t CellDataPtr = 0x0;
+            constexpr uintptr_t CellFlagBrush = 0x1;
+            constexpr uintptr_t CellFlagBuilding = 0x40;
+            constexpr uintptr_t CellFlagProp = 0x80;
+            constexpr uintptr_t CellFlagSpecialRegionMask = 0xC00;
+            constexpr uintptr_t CellFlagWall = 0x2;
+            constexpr uintptr_t CellFlags = 0x8;
+            constexpr uintptr_t CellSize = 0x710;
+            constexpr uintptr_t CellSizeInv = 0x714;
+            constexpr uintptr_t CellStride = 0x10;
+            constexpr uintptr_t Grid = 0x8;
+            constexpr uintptr_t WorldMaxX = 0xF8;
+            constexpr uintptr_t WorldMaxZ = 0x100;
+            constexpr uintptr_t WorldMinX = 0xEC;
+            constexpr uintptr_t WorldMinZ = 0xF4;
+        }
+        namespace CharacterDataStack {
+            constexpr uintptr_t ActiveGear = 0x9C;
+            constexpr uintptr_t ActiveModel = 0x18;
+            constexpr uintptr_t ActiveSkinId = 0x38;
+            constexpr uintptr_t Stack = 0x0;
+            constexpr uintptr_t StackCapacity = 0x10;
+            constexpr uintptr_t StackEnd = 0x8;
+        }
+        namespace SpellCastInfo {
+            constexpr uintptr_t HASHMAP_OFF_SENTINEL = 0x70;
+            constexpr uintptr_t HASHMAP_OFF_BUCKETS = 0x80;
+            constexpr uintptr_t HASHMAP_OFF_MASK = 0x98;
+            constexpr uintptr_t NODE_OFF_NEXT = 0x8;
+            constexpr uintptr_t NODE_OFF_KEY_STR = 0x10;
+            constexpr uintptr_t NODE_OFF_KEY_HASH = 0x18;
+            constexpr uintptr_t NODE_OFF_VALUE = 0x20;
+        }
+        namespace TimingPing {
+            constexpr uintptr_t OFF_A1_CONN_SLOT = 0x423;
+            constexpr uintptr_t OFF_A1_TREE_LINK = 0x0C8;
+            constexpr uintptr_t OFF_NODE_NEXT_LEFT = 0x000;
+            constexpr uintptr_t OFF_NODE_NEXT_RIGHT = 0x010;
+            constexpr uintptr_t OFF_NODE_END_FLAG = 0x019;
+            constexpr uintptr_t OFF_NODE_KEY = 0x020;
+            constexpr uintptr_t OFF_NODE_DATA = 0x028;
+            constexpr uintptr_t OFF_DATA_RTT_A = 328;
+            constexpr uintptr_t OFF_DATA_RTT_B = 344;
+            constexpr uintptr_t TIMING_STRIDE_QWORDS = 33;
+        }
+        namespace HeroManager {
+            constexpr uintptr_t OFF_HEROMGR_LIST = 0x8;
+            constexpr uintptr_t OFF_HEROMGR_COUNT = 0x10;
         }
     }
 }
